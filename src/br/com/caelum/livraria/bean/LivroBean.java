6ca0da77cdf1fd9b35ff5f563226d7353ff227b5
Marcos Barbosa <barbosa.mg@gmail.com>
2018-12-01 23:43:05 -0200

    Listando livros Usando conversores e validadores

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -36,10 +40,13 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
 	}
 	
 	public void gravarAutor() {
@@ -49,6 +56,20 @@ public class LivroBean {
 	
 	public List<Autor> getAutoresDoLivro() {
 		return this.livro.getAutores();
+	}
+	
+	public List<Livro> getListaLivros() {
+		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
+		return livros;
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		
+		if(!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+		
 	}
 
 }
