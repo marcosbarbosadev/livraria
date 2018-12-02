b731868a11407e3de289dc94a575e7aaca712836
Marcos Barbosa <barbosa.mg@gmail.com>
2018-12-01 22:59:37 -0200

    Adicionando funcionalidade de associar autores a um livro

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,19 +1,37 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
+	private Integer autorId;
 	private Livro livro = new Livro();
 
+	public Integer getAutorId() {
+		return autorId;
+	}
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
 	public Livro getLivro() {
 		return livro;
 	}
 
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
@@ -22,6 +40,15 @@ public class LivroBean {
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
 	}
 
 }
