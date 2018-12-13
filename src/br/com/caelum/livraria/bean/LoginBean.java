4d3af3ebcb2c74b708905a890cd60a68d1f0c0d2
Marcos Barbosa <barbosa.mg@gmail.com>
2018-12-12 23:17:13 -0200

    Implementando autorização do usuário Ao acessar uma página qualquer do sistema, verifica se o usuário está logado e redireciona para página de login caso ainda não tenha feito o login

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDAO;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -21,6 +22,8 @@ public class LoginBean {
 		boolean existe = new UsuarioDAO().existe(usuario);
 
 		if(existe) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", usuario);
 			return "livro?faces-redirect=true";
 		}
 		
