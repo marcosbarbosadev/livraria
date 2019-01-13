43ca95b625ad56fdaad4a29ada016d5713cf1c3c
Marcos Barbosa <barbosa.mg@gmail.com>
2019-01-13 21:02:50 -0200

    Definindo strategy GenerationType.IDENTITY para geração de chaves da entidade

--- src/br/com/caelum/livraria/modelo/Livro.java
+++ src/br/com/caelum/livraria/modelo/Livro.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -23,7 +24,7 @@ public class Livro implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String titulo;
